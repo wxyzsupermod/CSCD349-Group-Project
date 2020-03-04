77b3d90879828c0ff98f558b49f34ead47cdfb19
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-03 23:44:01 -0800

    This was the version of the HeroFactory that I came up with based on my Hero Factory but it's not really working properly

--- src/MonsterFactory.java
+++ src/MonsterFactory.java
@@ -1,4 +1,9 @@
 
-public class MonsterFactory {
-
+public class MonsterFactory{
+	
+	public static Monster createMonster(String name, int health_points, int attack_speed, int damage_min, int damage_max,
+			double hit_accuracy, double chanceToHeal, int minHeal, int maxHeal) {
+		return new Monster(name, health_points,attack_speed,damage_min, damage_max,hit_accuracy);
+	}
 }
+
