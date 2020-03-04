5e2592756b672922839bf37d541d9ca4bee9f3a9
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-03 23:36:41 -0800

    the most current version of main, currently only option 4 is properly hooked up sort of

--- src/DungeonAdventureDriver.java
+++ src/DungeonAdventureDriver.java
@@ -3,49 +3,51 @@ import java.util.*;
 
 public class DungeonAdventureDriver {
 	public static void main(String[] args) {
-		
+		Hero H1 = null;
 		int option;
 		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a name for your character:");
+		String name = kb.nextLine();
 		option = displayMenu(kb);
+		
+		
 		if(option ==1)
 		{
-			//call to the hero factory
-			//HeroFactory.createHero(Thief.name(),Thief.stats());
-		
+			 //call to the hero Factory
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " The kind Thief has "+  H1.getHealthPoints() + " in health points!"  );
 		}
-		else if( option ==2)
+		else if(option ==2)
 		{
-			//call to the hero factory
-			//HeroFactory.createHero(Sorceress.name(), Sorceress.stats());
-
+			 //call to the hero Factory
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " The Sorceress has "+  H1.getHealthPoints() + " in health points!"  );
 		}
-		else if(option == 3)
+		else if(option ==3)
 		{
-			//call to the HeroFactory
-			//HeroFactory.createHero(Sorceress.name(), Sorceress.stats());
+			 //call to the hero Factory
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " The Warrior "+  H1.getHealthPoints() + " in health points!"  );
 		}
 		else if(option ==4)
 		{
 			 //call to the hero Factory
-			 HeroFactory.createHero("melody", 100, 10, 35, 65,0.8);
-			
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " The Elf Wizard has "+  H1.getHealthPoints() + " in health points!"  );
+		}
+		else if(option ==5)
+		{
+			 //call to the hero Factory
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " aka Gandalf has "+  H1.getHealthPoints() + " in health points!"  );
+		}else if(option ==6)
+		{
+			 //call to the hero Factory
+			 H1 = HeroFactory.createHero(name, 100, 10, 35, 65,0.8);
+			 System.out.println("Creation of Hero successful : " + name  + " The Sorcerer has "+  H1.getHealthPoints() + " in health points!"  );
 		}
 	}
-	
-	
-	/*protected static int chooseAttack(Scanner kb)
-	{
-		
-		System.out.println("Please choose the type of attack:");
-		System.out.println("1. Special Attack " );
-		System.out.println("2. Regular attack ");
-		int choice = kb.nextInt();
-		kb.nextLine();
-		return choice;
-	}*/
-	
-	
-		private static int displayMenu(Scanner kb)
+	private static int displayMenu(Scanner kb)
 		{
 			
 			int choice;
@@ -54,6 +56,9 @@ public class DungeonAdventureDriver {
 			System.out.println("2. Sorceress ");
 			System.out.println("3. Warrior ");
 			System.out.println("4. Elf Wizard ");
+			System.out.println("5. Gandalf  ");
+			System.out.println("6. Socerer ");
+			
 
 			choice = kb.nextInt();
 			return choice;
