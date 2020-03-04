83e592c635f1c5e11e15da994441f554d1edea04
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-03 23:41:27 -0800

    This was the version of Hero that I came up with too, it's not perfect but seems to work ok

--- src/Hero.java
+++ src/Hero.java
@@ -1,4 +1,32 @@
+import java.util.Random;
 
-public class Hero {
-//haha corbin was here
-}
+public class Hero extends GameCharacter{
+	
+	public Hero(String name, int healthPoints, int attackSpeed, int damageRangeMin, int damageRangeMax,
+				double hitChance) {
+		super("melody", 25, 10, 5,4,2);
+		
+	}
+	
+	
+	public void takeDamage() {}
+	public void chooseAttack() {}
+	
+	public void attackMethod(GameCharacter opponent) {}
+	public void specialSkill(GameCharacter opponent) {}
+	
+	
+	public boolean isAlive( double healthPoints) {
+			if(healthPoints <= 0)
+			{
+				return true;
+			}
+			else 
+			{
+				return false;
+				
+			}
+		}
+	}
+
+	
