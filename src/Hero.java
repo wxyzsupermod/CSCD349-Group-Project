d458d6428fe06250d9c2a210540d355467e5a52e
wxyzsupermod <wxyzsupermod@gmail.com>
2020-03-03 18:00:02 -0800

    Update the Hero file to not be broken

--- src/Hero.java
+++ src/Hero.java
@@ -1,10 +1,7 @@
 
-<<<<<<< HEAD
+
 public interface Hero {
 	public boolean canDefend();
 	public void printNumTurns(GameCharacter opponent);
-=======
-public class Hero {
-//haha corbin was here
->>>>>>> MelodysBranch
+
 }
