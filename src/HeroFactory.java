ef8e7da2dbe3f3de13a9b667c2158b58a273fdee
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-03 23:39:11 -0800

    I tried to work on the HeroFactory and this is what I got to work with my code as is...

--- src/HeroFactory.java
+++ src/HeroFactory.java
@@ -1,4 +1,11 @@
 
-public class HeroFactory {
-
+public class HeroFactory{
+	
+	//public static ElfWizard createElfWizard(String name) {
+		//return new ElfWizard(name);
+	//}
+	public static Hero createHero(String name, int healthPoints, int attackSpeed, int damageMin, int damageMax,
+			double hitChance) {
+		return new Hero(name,healthPoints,attackSpeed,damageMin,damageMax,hitChance);
+	}
 }
