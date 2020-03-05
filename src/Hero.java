66ec181ef3bcaf55395fc48083ecc08d8822fd0f
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-04 21:30:56 -0800

    This is the Hero Interface and it works well with everything!

--- src/Hero.java
+++ src/Hero.java
@@ -1,32 +1,17 @@
 import java.util.Random;
 
-public class Hero extends GameCharacter{
-	
-	public Hero(String name, int healthPoints, int attackSpeed, int damageRangeMin, int damageRangeMax,
-				double hitChance) {
-		super("melody", 25, 10, 5,4,2);
-		
-	}
+public interface Hero extends GameCharacter{
 	
 	
-	public void takeDamage() {}
-	public void chooseAttack() {}
 	
-	public void attackMethod(GameCharacter opponent) {}
-	public void specialSkill(GameCharacter opponent) {}
+	public void takeDamage();
+	public void chooseAttack();
+	
+	public void regularAttack(GameCharacter opponent);
+	public void specialAttack(GameCharacter opponent);
 	
 	
-	public boolean isAlive( double healthPoints) {
-			if(healthPoints <= 0)
-			{
-				return true;
-			}
-			else 
-			{
-				return false;
-				
-			}
-		}
+	public boolean isAlive( double healthPoints);
 	}
 
 	
