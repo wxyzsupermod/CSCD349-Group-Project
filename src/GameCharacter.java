d6dc793632a8fb8315404f0093d7cb95cec63900
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-04 21:31:56 -0800

    This is the GameCharacter Interface and it works well with everything!

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -1,100 +1,15 @@
-import java.util.*;
 
-	public abstract class GameCharacter implements AttackBehavior
+
+	public interface GameCharacter extends AttackBehavior
 	{
-		protected String name;
-		protected int health_points;
-		protected int attack_speed;
-		protected int damage_min;
-		protected int damage_max;
-		protected double hit_accuracy;
-		AttackBehavior attack;
 		
-		public GameCharacter() 
-		{
-		
-		}
+		public abstract void regularAttack(GameCharacter opponent);
+		public abstract void specialAttack(GameCharacter opponent);
+		public boolean isAlive(double healthPoints);
+		public int getAttackSpeed();
+		public String getName();
 
-		public GameCharacter(String name, int health_points, int attack_speed, int damage_min, int damage_max,
-				double hit_accuracy)
-		{
-			this.name = name;
-			this.health_points = health_points;
-			this.attack_speed = attack_speed;
-			this.damage_min = damage_min;
-			this.damage_max = damage_max;
-			this.hit_accuracy = hit_accuracy;
-		}
-		public abstract void attackMethod(GameCharacter opponent);
-		public abstract void specialSkill(GameCharacter opponent);
-		public abstract boolean isAlive(double healthPoints);
-		
-		protected String getName()
-		{
-			return name;
-		}
-
-		protected void setName(String name) 
-		{
-			this.name = name;
-		}
-
-		protected  int getHealthPoints() 
-		{
-			return health_points;
-		}
-
-		public void setHealthPoints(int health_points) 
-		{
-			this.health_points = health_points;
-		}
-
-		public int getAttackSpeed()
-		{
-			return attack_speed;
-		}
-
-		public void setAttackSpeed(int attackSpeed) 
-		{
-			this.attack_speed = attackSpeed;
-		}
-
-		public int getDamageMin() 
-		{
-			return damage_min;
-		}
-
-		public void setDamageMin(int damage_min) 
-		{
-			this.damage_min = damage_min;
-		}
-
-		public int getDamageMax()
-		{
-			return damage_max;
-		}
-
-		public void setDamageMax(int damage_max) 
-		{
-			this.damage_max = damage_max;
-		}
-
-		public double getHitChance() 
-		{
-			return hit_accuracy;
-		}
-
-		public void setHitChance(double hit_accuracy)
-		{
-			this.hit_accuracy = hit_accuracy;
-		}
-		
-		@Override
-		public String toString()
-		{
-			return "Character is " + this.name + " with health points of " + this.health_points;
-		}
-		
+	
 	}
 
 
