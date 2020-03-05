9859cd3b3a9da6a27e4b1e7725e146b4110ce6d0
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-04 21:28:46 -0800

    This is the heroFactory class and it works well!

--- src/HeroFactory.java
+++ src/HeroFactory.java
@@ -1,11 +1,30 @@
 
 public class HeroFactory{
 	
-	//public static ElfWizard createElfWizard(String name) {
-		//return new ElfWizard(name);
-	//}
-	public static Hero createHero(String name, int healthPoints, int attackSpeed, int damageMin, int damageMax,
-			double hitChance) {
-		return new Hero(name,healthPoints,attackSpeed,damageMin,damageMax,hitChance);
+
+	public static Hero createElfWizard(String name)
+	{
+		return new ElfWizard(name);
 	}
+	public static Hero createWarrior(String name)
+	{
+		return new Warrior(name);
+	}
+	public static Hero createGandalf(String name)
+	{
+		return new Gandalf(name);
+	}
+	public static Hero createSorcerer(String name)
+	{
+		return new Sorcerer(name);
+	}
+	public static Hero createSorceress(String name)
+	{
+		return new Sorceress(name);
+	}
+	public static Hero createThief(String name)
+	{
+		return new Thief(name);
+	}
+	
 }
