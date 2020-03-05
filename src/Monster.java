ce99171b8e476fbb67d2299b09430546f71f7504
Melody Kinyon <mkinyon@eagles.ewu.edu>
2020-03-04 21:30:18 -0800

    This is the Monster Interface and it works well with everything!

--- src/Monster.java
+++ src/Monster.java
@@ -1,4 +1,17 @@
 
-public class Monster {
+		public interface Monster extends GameCharacter
+		{
+			
+		
+		
+		public void takeDamage() ;
+		public AttackBehavior chooseAttack();
+		public void regularAttack(GameCharacter opponent);
+		public void specialAttack(GameCharacter opponent);
+		
+		
+		public boolean isAlive( double healthPoints) ;
+				
+		}
 
-}
+
